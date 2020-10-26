d3d4f6c840ba13b705819e27b963dd09c7a153e4
Prakash <59432256+prakash090200@users.noreply.github.com>
2020-10-26 19:29:10 +0530

    ch

--- Maths/pascal Triangle.java
+++ Maths/pascal Triangle.java
@@ -9,10 +9,19 @@ class Main {
              int [] a=new int[i];
            if (i == 1) 
                System.out.println(1);
-             else { a[0]=1;a[a.length-1]=1;for(int j=1;j<a.length-1;j++) a[j]=b.get(j-1);b.clear();
-                 for(int j=0;j<a.length;j++) System.out.print(a[j]+" ");System.out.println();
-                 for(int j=1;j<a.length;j++) b.add(a[j]+a[j-1]);
-                 a=null;}
-             }
-     }}
+             else { a[0]=1;a[a.length-1]=1;
+             for(int j=1;j<a.length-1;j++) 
+             a[j]=b.get(j-1);
+             b.clear();
+         for(int j=0;j<a.length;j++)
+         System.out.print(a[j]+" ");
+         System.out.println();
+  for(int j=1;j<a.length;j++)
+  b.add(a[j]+a[j-1]);
+      a=null;
+      }
+             
+    }
+  }
+}
 
