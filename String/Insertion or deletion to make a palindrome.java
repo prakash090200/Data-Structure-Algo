744d8e84600fb4ad0ebb9b79a235e2d80a2bda5a
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-05 02:10:26 +0530

    insertion or deletion to make string a plaindrome

--- String/Insertion or deletion to make a palindrome.java
+++ String/Insertion or deletion to make a palindrome.java
@@ -33,6 +33,6 @@ class GFG {
 	            dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
 	        }
 	    }
-	    System.out.println(s1.length()-dp[n][n]);   // string length- lcs
+	    System.out.println(s1.length()-dp[n][n]);   // string length - (lcs)
 	}
 }
