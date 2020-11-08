0bd10f3b658f85ff0cfaff741c1a9b36b2986ad1
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-08 13:39:16 +0530

    Array Pair Sum Divisibility Problem

---
+++ hash/Array pair sum divisibilty (HashMap).java
@@ -0,0 +1,40 @@
+import java.lang.*;
+import java.io.*;
+
+
+///// Array Pair Sum Divisibility Problem 
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    int k=sc.nextInt();
+		    System.out.println(apsd(arr,n,k)?"True":"False");
+		}
+	}
+	static boolean apsd(int arr[],int n,int k){
+	    if(n%2==1)
+	    return false;
+	    HashMap<Integer,Integer> hm=new HashMap<>();
+	    for(int i=0;i<n;i++){
+	        int rem=arr[i]%k;
+	        if(!hm.containsKey(rem))
+	        hm.put(rem,1);
+	        else
+	        hm.put(rem,hm.get(rem)+1);
+	    }
+	    for(int i=0;i<n;i++){
+	        int rem=arr[i]%k;
+	        if(2*rem==k&&hm.get(rem) % 2 == 1)
+	        return false;
+	        else if (hm.get(k - rem) != hm.get(rem)&&rem!=0) 
+            return false;
+	    }
+	    return true;
+	}
+}
