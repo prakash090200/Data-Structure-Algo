6abc78d254974e441913b65b45f1d59bba5eeabc
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-11 17:29:18 +0530

    rotate the linked list

---
+++ Linked list/Rotate the linked list.java
@@ -0,0 +1,21 @@
+
+// rotate the linked list
+class Rotate{
+    
+    public Node rotate(Node head, int k) {
+       Node temp=head;
+       Node prev=null;
+       for(int i=0;i<k;i++){
+           prev=temp;
+           temp=temp.next;
+       }
+       if(temp==null)return head;
+       Node newhead=temp;
+       
+       while(newhead.next!=null)
+       newhead=newhead.next;
+       newhead.next=head;
+       prev.next=null;
+       return temp;
+    }        
+}
