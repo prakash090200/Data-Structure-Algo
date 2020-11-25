63079980fcfb295bbf52f54b3aa0ae0a79140c28
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-25 18:06:46 +0530

    Maximum of all subarrays of size k (Hashmap)

--- hash/Maximum of all subarrays of size k (Hashmap).java
+++ hash/Maximum of all subarrays of size k (Hashmap).java
@@ -1,12 +1,11 @@
-
-/// Count distinct elements in every window (hashmap)
 //// Maximum of all subarrays of size k (Hashmap)
+
+//// Count distinct elements in every window (hashmap)
 **************************************************************
 
-class  Solution
-{
-    ArrayList<Integer> countDistinct(int A[], int n, int k)
-    {
+class solve{
+    static ArrayList <Integer> max_of_subarrays(int A[], int n, int k)
+    {    
         ArrayList<Integer> a=new ArrayList<>();
         HashMap<Integer,Integer> h=new HashMap<>();
 
@@ -14,27 +13,27 @@ class  Solution
         int j=0;
      for(int i=0;i<k;i++)
      add(h,A[j++]);
-     a.add(h.size());
+     a.add(Collections.max(h.keySet()));
      for(int i=0;i<n-k;i++){
          delete(h,A[i]);
          add(h,A[j++]);
-         a.add(h.size());
+         a.add(Collections.max(h.keySet()));
          
      }
        return a;
        
     }
-   void add(HashMap<Integer,Integer> h,int g){
+
+  static void add(HashMap<Integer,Integer> h,int g){
         if(h.containsKey(g))
         h.put(g,h.get(g)+1);
         else
         h.put(g,1);
     }
-    void delete(HashMap<Integer,Integer> h,int g){
+    static void delete(HashMap<Integer,Integer> h,int g){
         if(h.get(g)==1)
         h.remove(g);
         else
         h.put(g,h.get(g)-1);
     }
 }
-
