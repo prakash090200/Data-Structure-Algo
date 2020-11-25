5bc806e73f5592d23f9538a933ef912103de0389
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-25 19:40:34 +0530

    Maximum of all subarrays of size k

--- hash/Maximum of all subarrays of size k (Hashmap).java
+++ hash/Maximum of all subarrays of size k (Hashmap).java
@@ -37,3 +37,31 @@ class solve{
         h.put(g,h.get(g)-1);
     }
 }
+**********************************************************
+//// Maximum of all subarrays of size k
+
+class solve{
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {    
+        Deque<Integer>q= new LinkedList<Integer>();
+        ArrayList<Integer>retVal = new ArrayList<>();
+        int i;
+        for(i=0;i<k;i++){
+            while(!q.isEmpty() && arr[i]>=arr[q.peekLast()])
+            q.removeLast();
+            q.addLast(i);
+        }
+        for(;i<n;i++){
+            retVal.add(arr[q.peek()]);
+            while(!q.isEmpty() && q.peek() <= i-k)
+            q.removeFirst();
+            while(!q.isEmpty() && arr[i]>=arr[q.peekLast()])
+            q.removeLast();
+            q.addLast(i);
+        }
+        retVal.add(arr[q.peek()]);
+        return retVal;
+        
+    }
+    
+}
