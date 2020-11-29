f541c861b783d56a825c456705253fa0f69ed657
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-11-29 18:04:04 +0530

    Check for BST

---
+++ tree/Check for BST.java
@@ -0,0 +1,22 @@
+//// Check for BST
+
+public class Tree
+{
+    boolean isBST(Node root)
+        {
+            return check(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        }
+    boolean check(Node root,int min,int max){
+        if(root==null)
+            return true;
+        boolean flag=false;
+        if(root.data>min && root.data<max){
+           
+          if(check(root.left,min,root.data) && check(root.right,root.data,max))
+           flag=true;
+         
+    }
+    
+    return flag;
+}
+}
