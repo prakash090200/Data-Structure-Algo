89ce26181b9c90c0b62462d0c271555d883fb26f
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-12-05 15:35:26 +0530

    Does robot moves circular

---
+++ String/Does robot moves circular.java
@@ -0,0 +1,58 @@
+////Does robot moves circular 
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- >0){
+		    String s=sc.next().toUpperCase().trim();
+		    int x=0;
+		    int y=0;
+		    int N=0;
+		    int W=3;
+		    int E=1;
+		    int S=2;
+		    int dir=N;
+		    for(int i=0;i<s.length();i++){
+		        char c=s.charAt(i);
+		        
+		        if(c=='R')
+		            dir=(int)(dir+1)%4;
+		           
+		        
+		        else if(c=='L')
+		            dir=(int)(4+dir-1)%4;
+		           
+		        
+		        else if(c=='G'){
+		      
+		            if(dir==N)
+		                y=y+1;
+		                
+		            
+		            if(dir==W)
+		                x=x-1;
+		                
+		            
+		            if(dir==S)
+		                y=y-1;
+		                	            
+		            if(dir==E)
+		                x=x+1;
+		               
+		        } 
+		            
+		        }
+		        if(x==0 && y==0)
+		        System.out.println("Circular");
+		        else
+		        System.out.println("Not Circular");
+		        
+		    
+		}
+	}
+}
