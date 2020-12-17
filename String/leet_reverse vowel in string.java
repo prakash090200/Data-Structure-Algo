6aa9b41eed8e98c07f700e28ff99d2f809e00379
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-12-17 15:25:04 +0530

    leet_rotate String left side leet

---
+++ String/leet_reverse vowel in string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String reverseVowels(String s) {
+        if(s.isEmpty())
+            return new String();
+        
+       char []c=s.toCharArray();
+        int start=0;
+        int end=c.length-1;
+        while(startend){
+            if(vowel(c[start]) && vowel(c[end])){
+                char temp=c[start];
+                c[start++]=c[end];
+                c[end--]=temp;
+                
+            }
+            else if(!vowel(c[start]))
+            start++;
+        
+            
+            else if(!vowel(c[end]))
+                end--;
+        }
+        s=new String(c);
+        return s;
+    }
+    public boolean vowel(char c){
+        c=Character.toLowerCase(c);
+          return (c=='a' c=='e' c=='i' c=='o' c=='u' );
+             
+        
+    }
+}
---
+++ String/leet_rotate String left side .java
@@ -0,0 +1,30 @@
+////leet_rotate String left side 
+
+class Solution {
+   
+    public boolean rotateString(String A, String B) {
+        
+        if(A.length()!=B.length())
+            return false;
+        if(A.length()==0)
+            return true;
+        
+        loop1:
+       for(int i=0;i<A.length();i++){
+           for(int j=0;j<A.length();j++){
+               if(A.charAt((i+j)%A.length())!=B.charAt(j))
+                   continue loop1;
+                 
+           }
+           return true;
+           }
+           return false;
+}
+}
+
+**************************************
+class Solution {
+    public boolean rotateString(String A, String B) {
+        return A.length() == B.length() && (A + A).contains(B);
+    }
+}
