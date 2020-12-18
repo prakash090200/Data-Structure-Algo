6687de633e5e13ce074b969f8ddc6cd55c44d61e
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-12-18 17:55:34 +0530

    leet Remove Duplicate Letters  in lexicographical order

---
+++ String/leet Remove Duplicate Letters  in lexicographical order.java
@@ -0,0 +1,28 @@
+//Remove Duplicate Letters  in lexicographical order
+
+class Solution {
+    public String removeDuplicateLetters(String s) {
+         int count[]=new int[26];
+        boolean status[]=new boolean[26];
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
+        }
+        sb.append('0');
+        for(char c:s.toCharArray()){
+            count[c-'a']--;
+            
+            if(!status[c-'a']){
+                 char lc=sb.charAt(sb.length()-1);
+                while(c<lc && count[lc-'a']>0){
+                    sb.deleteCharAt(sb.length()-1);
+                    status[lc-'a']=false;
+                    lc=sb.charAt(sb.length()-1);
+                }
+                sb.append(c);
+                status[c-'a']=true;
+            }
+        }
+        return sb.substring(1).toString();
+    }
+}
