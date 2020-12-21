f3577ec80bb1b6d4588bdf4bbc68f97a5470cf36
prakash <prakash.kumar2018a@vitstudent.ac.in>
2020-12-21 12:49:36 +0530

    leet isomorphic string

--- String/leet Remove Duplicate Letters  in lexicographical order.java
+++ String/leet Remove Duplicate Letters  in lexicographical order.java
@@ -1,7 +1,5 @@
 //Remove Duplicate Letters  in lexicographical order
-
 // smallest-subsequence-of-distinct-characters
-
 
 
 class Solution {
@@ -12,17 +10,19 @@ class Solution {
         for(char c:s.toCharArray()){
             count[c-'a']++;
         }
+         
         sb.append('0');
         for(char c:s.toCharArray()){
             count[c-'a']--;
             
             if(!status[c-'a']){
                  char lc=sb.charAt(sb.length()-1);
-                while(c<lc && count[lc-'a']>0){
+                    while(c<lc && count[lc-'a']>0){
                     sb.deleteCharAt(sb.length()-1);
                     status[lc-'a']=false;
                     lc=sb.charAt(sb.length()-1);
-                }
+               }
+
                 sb.append(c);
                 status[c-'a']=true;
             }
---
+++ String/leet_isomorphic-strings.java
@@ -0,0 +1,44 @@
+///// isomorphic-strings
+
+
+---> best complexity
+
+class Solution {
+   
+   public boolean isIsomorphic(String s, String t) {
+		int[] s1 = new int[128];
+		int[] s2 = new int[128];
+		for (int i = 0; i < s.length(); i++) {
+			char c1 = s.charAt(i);
+			char c2 = t.charAt(i);
+			if (s1[c1] != s2[c2]) {
+				return false;
+			}
+			s1[c1] = i + 1;
+			s2[c2] = i + 1;
+		}
+		return true;
+	}
+}
+******************************************************
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        
+        if(s.length()!=t.length())
+            return false;
+       
+        HashMap<Character,Character> h=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ca=s.charAt(i);
+            char cb=t.charAt(i);
+            if(!h.containsKey(ca) && !h.containsValue(cb))
+                h.put(ca,cb);
+            else if(!h.containsKey(ca) && h.containsValue(cb) || !h.get(ca).equals(cb) )
+                return false;
+        }
+        return true;
+        
+    }
+}
