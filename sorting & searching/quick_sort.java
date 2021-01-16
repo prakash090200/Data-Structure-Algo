f229e1a81276e44301b1e9be045399b2b9d18dd9
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-01-16 23:19:03 +0530

    quick sort optimized

--- sorting & searching/quick_sort.java
+++ sorting & searching/quick_sort.java
@@ -35,3 +35,31 @@ class Solution {
 }
 
 ****************************
+
+optimized worst case of quick sort:
+
+/IMP/  
+
+void quickSort(int arr[], int low, int high) 
+{ 
+    while (low < high) 
+    { 
+        
+        int pi = partition(arr, low, high); 
+  
+        // If left part is smaller, then recur for left 
+        // part and handle right part iteratively 
+
+        if (pi - low < high - pi) 
+        { 
+            quickSort(arr, low, pi - 1); 
+            low = pi + 1; 
+        } 
+  
+        else
+        { 
+            quickSort(arr, pi + 1, high); 
+            high = pi - 1; 
+        } 
+    } 
+} 
