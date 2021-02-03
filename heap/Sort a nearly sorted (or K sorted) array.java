2c832ce9900909b9ae63d48f0a48be6932378c35
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-02-03 11:05:22 +0530

    k sorted elememt in array

---
+++ heap/Sort a nearly sorted (or K sorted) array.java
@@ -0,0 +1,24 @@
+ // min heap
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+ 
+        // add first k + 1 items to the min heap
+        for(int i = 0; i < k + 1; i++)
+        {
+            priorityQueue.add(arr[i]);
+        }
+ 
+        int index = 0;
+        for(int i = k + 1; i < n; i++) 
+        {
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+            priorityQueue.add(arr[i]);
+        }
+ 
+        Iterator<Integer> itr = priorityQueue.iterator();
+ 
+        while(itr.hasNext()) 
+        {
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+        }
