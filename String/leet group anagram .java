7b60ac855fa9c7c980b9a28818ebaf88bc7d2eb6
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-04-06 12:22:45 +0530

    save

--- String/leet group anagram .java
+++ String/leet group anagram .java
@@ -1,7 +1,12 @@
+https://leetcode.com/problems/group-anagrams/
+
+///  Group Anagrams
+
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
      
-        Map<String , List<String> > map = new  HashMap();
+        Map<String , List<String> > map = new  HashMap<>();
     
         for( String st : strs ){
             char ch[] = st.toCharArray();
@@ -22,10 +27,46 @@ class Solution {
             
         }
         
-        List<List<String>> list = new ArrayList();
+        List<List<String>> list = new ArrayList<>();
         
-        for( String  sb  : map.keySet() ){
-            list.add(map.get(sb));
+        for( List<String> a  : map.values() ){
+            list.add(a);
+        }
+        return list;
+    }
+}
+
+
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+     
+        Map<String , List<String> > map = new  HashMap<>();
+    
+        for( String st : strs ){
+            char ch[] = st.toCharArray();
+            
+            Arrays.sort(ch);
+            
+            String sb = new String(ch);
+            
+            if( map.containsKey(sb) ){
+                map.get(sb).add(st);
+            }
+            else{
+                List<String> l = new ArrayList();
+                l.add(st);
+                map.put(sb, l);
+            }
+            
+            
+        }
+        
+        List<List<String>> list = new ArrayList<>();
+        
+        for( List<String> a  : map.values() ){
+            list.add(a);
         }
         return list;
     }
