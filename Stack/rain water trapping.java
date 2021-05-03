784c6ca00df300bb0ef19927402f65a5a3f9b5ec
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-03 20:11:58 +0530

    save

--- Stack/rain water trapping.java
+++ Stack/rain water trapping.java
@@ -1,5 +1,45 @@
- */ // rain water trapping
-/*class Main{
+https://leetcode.com/problems/trapping-rain-water/submissions/
+
+https://www.geeksforgeeks.org/trapping-rain-water/
+
+time:O(N)
+SPACE:O(1)
+
+
+class Solution {
+    public int trap(int[] arr) {
+        int low=0;
+        int high=arr.length-1;
+        int left=0;
+        int right=0;
+        int res=0;
+        while(low<high){
+            if(arr[low]<arr[high]){
+                if(arr[low]>left)
+                    left=arr[low];
+                else
+                    res+=left-arr[low];
+        
+                low++;
+            }
+            else{
+                if(arr[high]>right)
+                    right=arr[high];
+                else
+                    res+=right-arr[high];
+                high--;
+            }
+        }
+        return res;
+    }
+}
+
+**************************************************
+
+time:O(N)
+SPACE:O(N)
+
+class Main{
   public static void main(String [] args){
     int arr[]={3, 0, 2, 0, 4};
     int [] right=new int[5];
