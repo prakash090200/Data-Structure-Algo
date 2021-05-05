12ef9d853a392027744ec14f04380c8be3edbe4e
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-05 20:56:42 +0530

    save

---
+++ String/varity100_count of string withequal 0 and 1.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/count-binary-substrings/
+///Count Binary Substrings
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ans = 0, prev = 0, cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                ans += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            } else {
+                cur++;
+            }
+        }
+        return ans + Math.min(prev, cur);
+    }
+}
