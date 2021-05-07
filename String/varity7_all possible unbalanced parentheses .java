cb2bf8e0c2590107052584c7ff54d720fae16a0d
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-07 10:24:22 +0530

    save

--- String/varity7_all possible unbalanced parentheses .java
+++ String/varity7_all possible unbalanced parentheses .java
@@ -2,6 +2,61 @@ https://leetcode.com/problems/remove-invalid-parentheses/submissions/
 
 //Remove Invalid Parentheses
 
+//BEST
+
+class Solution {
+ public List<String> removeInvalidParentheses(String s) {
+    int rmL = 0, rmR = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '(') {
+            rmL++;
+        } else if (s.charAt(i) == ')') {
+            if (rmL != 0) {
+                rmL--;
+            } else {
+                rmR++;
+            }
+        }
+    }
+    Set<String> res = new HashSet<>();
+    dfs(s, 0, res, new StringBuilder(), rmL, rmR, 0);
+    return new ArrayList<String>(res);
+}
+
+public void dfs(String s, int i, Set<String> res, StringBuilder sb, int rmL, int rmR, int open) {
+    if (rmL < 0 || rmR < 0 || open < 0) {
+        return;
+    }
+    if (i == s.length()) {
+        if (rmL == 0 && rmR == 0 && open == 0) {
+            res.add(sb.toString());
+        }        
+        return;
+    }
+
+    char c = s.charAt(i); 
+    int len = sb.length();
+
+    if (c == '(') {
+        dfs(s, i + 1, res, sb, rmL - 1, rmR, open);		    // not use (
+    	dfs(s, i + 1, res, sb.append(c), rmL, rmR, open + 1);       // use (
+
+    } else if (c == ')') {
+        dfs(s, i + 1, res, sb, rmL, rmR - 1, open);	            // not use  )
+    	dfs(s, i + 1, res, sb.append(c), rmL, rmR, open - 1);  	    // use )
+
+    } else {
+        dfs(s, i + 1, res, sb.append(c), rmL, rmR, open);	
+    }
+
+    sb.setLength(len);        
+}
+}
+
+
+********************************************************************************************
+
+
 class Solution {
      public List<String> removeInvalidParentheses(String s) {
         List<String> ret = new ArrayList<>();
