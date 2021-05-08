7b889fa19e46b76c48ea986545155a8b1b2cf273
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-08 14:40:26 +0530

    save

--- MATRIX/varity2_Search a 2D Matrix II.java
+++ MATRIX/varity2_Search a 2D Matrix II.java
@@ -4,7 +4,7 @@ https://www.youtube.com/watch?v=VS0BcOiKaGI&ab_channel=AdityaVerma
 
 //Search a 2D Matrix II
 
-
+BEST**
 
 class Solution {
     public boolean searchMatrix(int[][] mat, int x) {
@@ -26,5 +26,40 @@ class Solution {
         }
         return false;
         
+    }
+}
+
+*********************************************
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        for(int i = 0; i < r; ++i)
+            {
+                  
+             if(Arrays.binarySearch(matrix[i],target)>=0)
+                 return true;
+        }
+        return false;
+    }
+}
+************************************************
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 0 ; i < m ; i++) {
+            if(matrix[i][0] > target) return false;
+            int low = 0;
+            int high  = n - 1;
+            
+            while(low <= high ) {
+                int mid = (high  + low ) / 2;
+                if(matrix[i][mid] == target) return true;
+                if(matrix[i][mid] > target)  high  = mid - 1;
+                if(matrix[i][mid] < target)  low = mid + 1;
+            }
+        }
+        return false;
     }
 }
--- MATRIX/varity2_Search a 2D Matrix.java
+++ MATRIX/varity2_Search a 2D Matrix.java
@@ -22,3 +22,19 @@ class Solution {
         return false;
     }
 }
+
+**********************************
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        for(int i = 0; i < r; ++i)
+            {
+                  
+             if(Arrays.binarySearch(matrix[i],target)>=0)
+                 return true;
+        }
+        return false;
+    }
+}
