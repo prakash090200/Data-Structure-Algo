cf2e273bbfaa01eac2666cf122e564d824387695
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-08 15:07:09 +0530

    save

--- MATRIX/Median in a row-wise sorted Matrix.java
+++ MATRIX/Median in a row-wise sorted Matrix.java
@@ -18,3 +18,58 @@ class Solution {
         return arr[m];       
     }
 }
+*****************************************************
+
+BEST******
+
+class Solution {
+    int median(int m[][], int r, int c) {
+       int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+          
+        for(int i=0; i<r ; i++)
+        {
+              
+           
+            if(m[i][0] < min)
+                min = m[i][0];
+            
+            if(m[i][c-1] > max)
+                max = m[i][c-1];
+        }
+          
+        int desired = (r * c + 1) / 2;
+        while(min < max)
+        {
+            int mid = min + (max - min) / 2;
+            int place = 0;
+            int get = 0;
+              
+            // Find count of elements smaller than mid
+            for(int i = 0; i < r; ++i)
+            {
+                  
+                get = Arrays.binarySearch(m[i],mid);
+                  
+                
+                if(get < 0)
+                    get = Math.abs(get) - 1;
+                  
+                
+                else
+                {
+                    while(get < m[i].length && m[i][get] == mid)
+                        get += 1;
+                }
+                  
+                place = place + get;
+            }
+              
+            if (place < desired)
+                min = mid + 1;
+            else
+                max = mid;
+        }
+        return min;
+    }   
+    }
