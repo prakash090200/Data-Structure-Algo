445a14bf6d8a923b9d56262eb19497b6a7532d28
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-08 12:16:38 +0530

    save

---
+++ MATRIX/varity2_search element in a sorted matrix.java
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/search-a-2d-matrix/submissions/
+//Search a 2D Matrix
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 0 ; i < m ; i++) {
+            if(matrix[i][0] > target) return false;
+            int low = 0;
+            int high  = n - 1;
+            
+            while(low <= high ) {
+                int mid = (high  + low ) / 2;
+                if(matrix[i][mid] == target) return true;
+                if(matrix[i][mid] > target)  high  = mid - 1;
+                if(matrix[i][mid] < target)  low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
