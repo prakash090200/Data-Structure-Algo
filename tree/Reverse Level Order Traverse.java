523879089bcbf6f9b9ee57f65c3621d906d03149
prakash <prakash.kumar2018a@vitstudent.ac.in>
2021-05-10 21:28:34 +0530

    save

--- tree/Reverse Level Order Traverse.java
+++ tree/Reverse Level Order Traverse.java
@@ -1,5 +1,51 @@
 https://leetcode.com/problems/binary-tree-level-order-traversal-ii/submissions/
 
+
+// FOr interview - using STACK AND QUEUE
+
+TIME : O(N)
+SPACE :O(N)
+
+class BinaryTree 
+{
+    Node root;
+ 
+    void reverseLevelOrder(Node node) 
+    {
+        Stack<Node> S = new Stack();
+        Queue<Node> Q = new LinkedList();
+        Q.add(node);
+   
+        while (Q.isEmpty() == false) 
+        {
+           
+            node = Q.peek();
+            Q.remove();
+            S.push(node);
+   
+          
+            if (node.right != null)
+               
+                Q.add(node.right); 
+                  
+            
+            if (node.left != null)
+                Q.add(node.left);
+        }
+   
+        
+        while (S.empty() == false) 
+        {
+            node = S.peek();
+            System.out.print(node.data + " ");
+            S.pop();
+        }
+    }
+
+
+***********************************************************
+
+
 class Solution {
     List<List<Integer>> retVal = new ArrayList<List<Integer>>();
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
